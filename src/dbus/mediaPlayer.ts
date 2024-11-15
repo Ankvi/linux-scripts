e9899e6db4bd3c412ef849cb766e2f696548fb80
Andreas Kvist <kvistcake@gmail.com>
2024-11-15 07:00:16 +0100

    WIP brave mpris handling and listing dbus names

--- src/dbus/mediaPlayer.ts
+++ src/dbus/mediaPlayer.ts
@@ -1,14 +1,18 @@
+import { Command } from "commander";
 import type { ClientInterface, ProxyObject, Variant } from "dbus-next";
-import { getSessionBus } from "./sessionBus";
+import { DBUS_PROPERTIES_INTERFACE, getSessionBus } from "./sessionBus";
 import type { Player, PlayerVariantInfo } from "./types";
 
 const MPRIS_OBJECT_PATH = "/org/mpris/MediaPlayer2";
-const DBUS_PROPERTIES_INTERFACE = "org.freedesktop.DBus.Properties";
 const DBUS_PLAYER_INTERFACE = "org.mpris.MediaPlayer2.Player";
 const SPOTIFY_PLAYER_INTERFACE = "org.mpris.MediaPlayer2.spotify";
 
 class MediaPlayer {
     private static _spotify: MediaPlayer;
+
+    static async getBrave() {
+        const sessionBus = getSessionBus();
+    }
 
     static async getSpotify() {
         if (!MediaPlayer._spotify) {
@@ -66,6 +70,11 @@ class MediaPlayer {
         this._player.Previous();
     }
 }
+
+export const media = new Command("media");
+
+const spotify = media.command("spotify");
+spotify.command("play").action(async () => {});
 
 if (import.meta.main) {
     const player = await MediaPlayer.getSpotify();
--- src/dbus/sessionBus.ts
+++ src/dbus/sessionBus.ts
@@ -1,7 +1,45 @@
 import * as dbus from "dbus-next";
 let sessionBus: dbus.MessageBus | undefined;
 
+export const DBUS_INTERFACE = "org.freedesktop.DBus";
+export const DBUS_PATH = "/org/freedesktop/DBus";
+export const DBUS_PROPERTIES_INTERFACE = `${DBUS_INTERFACE}.Properties`;
+
 export function getSessionBus(): dbus.MessageBus {
     sessionBus ??= dbus.sessionBus();
     return sessionBus;
+}
+
+export type ListSessionBusNamesArgs = {
+    path?: string;
+    interface?: string;
+};
+
+// dbus-send --print-reply --dest=org.freedesktop.DBus  /org/freedesktop/DBus org.freedesktop.DBus.ListNames
+export function listSessionBusNames(args?: ListSessionBusNamesArgs) {
+    const sessionBus = getSessionBus();
+    const methodCall = new dbus.Message({
+        destination: DBUS_INTERFACE,
+        path: args?.path ?? DBUS_PATH,
+        interface: args?.interface ?? DBUS_INTERFACE,
+        member: "ListNames",
+    });
+
+    return new Promise((resolve, reject) => {
+        const timeout = setTimeout(() => {
+            reject("timeout");
+        }, 3000);
+
+        sessionBus.once("message", (msg) => {
+            resolve(msg);
+            clearTimeout(timeout);
+        });
+
+        sessionBus.call(methodCall);
+    });
+}
+
+if (import.meta.main) {
+    const result = await listSessionBusNames();
+    console.log(result);
 }
